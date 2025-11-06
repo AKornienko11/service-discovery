2fb0bb0007bf19f5be6c3a92f02382f71ae8ed75
AKornienko11 <Kornienko.srs@mail.ru>
2025-11-06 17:12:22 +0500

    Service-discovery

--- skript.sql
+++ skript.sql
@@ -5,4 +5,8 @@ url varchar
 );
 
 insert into  links_properties (id, url)
-values (1, 'http://localhost:8083/api/user/');
+values (1, 'http://localhost:8084/api/user/get/'),
+       (2, 'http://localhost:8084/api/user/create'),
+       (3, 'http://localhost:8084/api/user/update'),
+       (4, 'http://localhost:8084/api/user/delete/'),
+       (5, 'http://localhost:8085/sending/send');
